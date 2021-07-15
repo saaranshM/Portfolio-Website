9c7f5f05051a32414b8ed9a9cbb7d157145ee73a
saaranshM <saaransh.menon2000@gmail.com>
2021-07-15 21:18:19 +0530

    header animations done

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,24 +1,34 @@
 <template>
   <header class="header">
-    <SvgIcon class="header__logo" name="colored-logo"></SvgIcon>
+    <transition appear appear-active-class="navigation-logo-animation">
+      <SvgIcon class="header__logo" name="colored-logo"></SvgIcon>
+    </transition>
     <nav class="navigation">
       <ul class="navigation__list">
-        <li class="navigation__item">
-          <span class="navigation__number">01.</span
-          ><a href="#" class="navigation__link">About</a>
-        </li>
-        <li class="navigation__item">
-          <span class="navigation__number">02.</span>
-          <a href="#" class="navigation__link">Projects</a>
-        </li>
-        <li class="navigation__item">
-          <span class="navigation__number">03.</span
-          ><a href="#" class="navigation__link">Experience</a>
-        </li>
-        <li class="navigation__item">
-          <span class="navigation__number">04.</span
-          ><a href="#" class="navigation__link">Contact</a>
-        </li>
+        <transition appear appear-active-class="navigation-fade-in-1">
+          <li class="navigation__item">
+            <span class="navigation__number">01.</span
+            ><a href="#" class="navigation__link">About</a>
+          </li>
+        </transition>
+        <transition appear appear-active-class="navigation-fade-in-2">
+          <li class="navigation__item">
+            <span class="navigation__number">02.</span>
+            <a href="#" class="navigation__link">Projects</a>
+          </li>
+        </transition>
+        <transition appear appear-active-class="navigation-fade-in-3">
+          <li class="navigation__item">
+            <span class="navigation__number">03.</span
+            ><a href="#" class="navigation__link">Experience</a>
+          </li>
+        </transition>
+        <transition appear appear-active-class="navigation-fade-in-4">
+          <li class="navigation__item">
+            <span class="navigation__number">04.</span
+            ><a href="#" class="navigation__link">Contact</a>
+          </li>
+        </transition>
       </ul>
     </nav>
   </header>
@@ -28,7 +38,12 @@
 import SvgIcon from "@/components/common/SvgIcon";
 export default {
   name: "Header",
-  components: { SvgIcon }
+  components: { SvgIcon },
+  data() {
+    return {
+      show: false
+    };
+  }
 };
 </script>
 
