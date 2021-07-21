713c48982dd840e016ef088805845cadc6335463
saaranshM <saaransh.menon2000@gmail.com>
2021-07-21 20:07:43 +0530

    added icons to footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,6 +1,32 @@
 <template>
   <footer class="footer">
     <div>
+      <ul class="footer__icons">
+        <li class="footer__list">
+          <a href="#">
+            <SvgIcon class="footer__icon" name="github"></SvgIcon
+          ></a>
+        </li>
+        <li>
+          <a href="#">
+            <SvgIcon class="footer__icon" name="linkedin"></SvgIcon
+          ></a>
+        </li>
+
+        <li class="footer__list">
+          <a href="#"> <SvgIcon class="footer__icon" name="insta"></SvgIcon></a>
+        </li>
+        <li class="footer__list">
+          <a href="#">
+            <SvgIcon class="footer__icon" name="twitter"></SvgIcon
+          ></a>
+        </li>
+        <li class="footer__list">
+          <a href="#">
+            <SvgIcon class="footer__icon medium-icon" name="medium"></SvgIcon
+          ></a>
+        </li>
+      </ul>
       <div class="footer__main">Made with <span>♥</span> by me.</div>
       <div class="footer__sub">
         Design inspired by
@@ -13,8 +39,10 @@
 </template>
 
 <script>
+import SvgIcon from "../common/SvgIcon";
 export default {
-  name: "Footer"
+  name: "Footer",
+  components: { SvgIcon }
 };
 </script>
 
