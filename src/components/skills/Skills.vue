dc427cb4103b3e460e103aef4d24ee343ac67077
saaranshM <saaransh.menon2000@gmail.com>
2021-07-21 16:04:54 +0530

    responsive changes

--- src/components/skills/Skills.vue
+++ src/components/skills/Skills.vue
@@ -1,24 +1,36 @@
 <template>
   <section class="skills">
-    <h2 class="numbered-heading">
-      <span class="heading-number">02.</span>
-      Skills
-    </h2>
-    <div class="skills__container">
-      <ul class="skills__grid">
-        <li v-for="(skill, index) in skills" :key="index" class="skills__item">
-          <h4 class="skills__item-heading">{{ skill.name }}</h4>
-          <div class="skills__item-progress">
-            <div
-              class="skills__item-progress-bar"
-              :style="`--width: ${skill.percentage}`"
-            ></div>
-            <p class="skills__item-progress-percent">
-              {{ skill.percentage }}
-            </p>
-          </div>
-        </li>
-      </ul>
+    <div
+      v-observe-visibility="{ callback: isViewableNow, once: true }"
+      :class="{
+        'visible animate__animated animate__fadeInUp visible': showAnimation,
+        invisible: !showAnimation
+      }"
+    >
+      <h2 class="numbered-heading">
+        <span class="heading-number">02.</span>
+        Skills
+      </h2>
+      <div class="skills__container">
+        <ul class="skills__grid">
+          <li
+            v-for="(skill, index) in skills"
+            :key="index"
+            class="skills__item"
+          >
+            <h4 class="skills__item-heading">{{ skill.name }}</h4>
+            <div class="skills__item-progress">
+              <div
+                class="skills__item-progress-bar"
+                :style="`--width: ${skill.percentage}`"
+              ></div>
+              <p class="skills__item-progress-percent">
+                {{ skill.percentage }}
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
