aa908a1276059116cdce479af29b083fede4f115
saaranshM <saaransh.menon2000@gmail.com>
2021-07-22 10:36:29 +0530

    added social links

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -3,26 +3,31 @@
     <div>
       <ul class="footer__icons">
         <li class="footer__list">
-          <a href="#">
+          <a href="https://github.com/saaranshM" target="_blank">
             <SvgIcon class="footer__icon" name="github"></SvgIcon
           ></a>
         </li>
         <li>
-          <a href="#">
+          <a
+            href="https://www.linkedin.com/in/saaransh-sunil-menon/"
+            target="_blank"
+          >
             <SvgIcon class="footer__icon" name="linkedin"></SvgIcon
           ></a>
         </li>
 
         <li class="footer__list">
-          <a href="#"> <SvgIcon class="footer__icon" name="insta"></SvgIcon></a>
+          <a href="https://www.instagram.com/saaransh_28/" target="_blank">
+            <SvgIcon class="footer__icon" name="insta"></SvgIcon
+          ></a>
         </li>
         <li class="footer__list">
-          <a href="#">
+          <a href="https://twitter.com/saaransh_28" target="_blank">
             <SvgIcon class="footer__icon" name="twitter"></SvgIcon
           ></a>
         </li>
         <li class="footer__list">
-          <a href="#">
+          <a href="https://saaransh-menon2000.medium.com/" target="_blank">
             <SvgIcon class="footer__icon medium-icon" name="medium"></SvgIcon
           ></a>
         </li>
@@ -30,7 +35,11 @@
       <div class="footer__main">Made with <span>♥</span> by me.</div>
       <div class="footer__sub">
         Design inspired by
-        <a href="#" class="underlined-link" :style="'--bottom: 0'"
+        <a
+          href="https://brittanychiang.com/"
+          target="_blank"
+          class="underlined-link"
+          :style="'--bottom: 0'"
           >brittanychiang.com</a
         >
       </div>
--- src/components/sidebar/SidebarLeft.vue
+++ src/components/sidebar/SidebarLeft.vue
@@ -3,28 +3,31 @@
     <transition appear appear-active-class="sidebar-left-animation">
       <ul class="sidebar-left__icons">
         <li class="sidebar-left__list">
-          <a href="#">
+          <a href="https://github.com/saaranshM" target="_blank">
             <SvgIcon class="sidebar-left__icon" name="github"></SvgIcon
           ></a>
         </li>
         <li>
-          <a href="#">
+          <a
+            href="https://www.linkedin.com/in/saaransh-sunil-menon/"
+            target="_blank"
+          >
             <SvgIcon class="sidebar-left__icon" name="linkedin"></SvgIcon
           ></a>
         </li>
 
         <li class="sidebar-left__list">
-          <a href="#">
+          <a href="https://www.instagram.com/saaransh_28/" target="_blank">
             <SvgIcon class="sidebar-left__icon" name="insta"></SvgIcon
           ></a>
         </li>
         <li class="sidebar-left__list">
-          <a href="#">
+          <a href="https://twitter.com/saaransh_28" target="_blank">
             <SvgIcon class="sidebar-left__icon" name="twitter"></SvgIcon
           ></a>
         </li>
         <li class="sidebar-left__list">
-          <a href="#">
+          <a href="https://saaransh-menon2000.medium.com/" target="_blank">
             <SvgIcon
               class="sidebar-left__icon medium-icon"
               name="medium"
