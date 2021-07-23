5649fa93fd9af54f02ec712a37152184bc4ca47a
saaranshM <saaransh.menon2000@gmail.com>
2021-07-23 11:50:17 +0530

    added resume button to sidenav and fixed  ham icon issue

--- src/components/sidenav/Sidenav.vue
+++ src/components/sidenav/Sidenav.vue
@@ -18,6 +18,14 @@
           <span>04. </span>
           <a v-scroll-to="'#contact'" href="#contact">Contact</a>
         </li>
+        <li class="sidenav__item">
+          <a
+            href="https://firebasestorage.googleapis.com/v0/b/vue-express-54b2e.appspot.com/o/RESUME%2FMy%20Resume.pdf?alt=media&token=84b70296-7c92-4444-900b-c707961a6e30"
+            target="_blank"
+            class="sidenav__resume"
+            >Resume</a
+          >
+        </li>
       </ol>
     </nav>
   </div>
